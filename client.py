a938725ad106950c9a7b5421d2d80b0cffeda5e1
Kuba <middleofdreams@gmail.com>
2010-01-09 18:48:40 +0100

    Dzialaja statusy

--- client.py
+++ client.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import pygtk,gtk,gtk.glade,xmpp,send,sys
+import pygtk,gtk,gtk.glade,xmpp,sys,pango
 from connection import connection
 import keys
+import send
 
 class okno:
 	def __init__(self):
@@ -18,19 +19,37 @@ class okno:
 		#po zamknięciu okna - kończymy program
 		
 		#pobranie obiektow z glade i przypisywanie ich do zmiennych:
+		
 		self.menu=self.wTree.get_widget("menubar1")
 		self.list=self.wTree.get_widget("treeview1")
-		self.statusbar=self.wTree.get_widget("comboboxentry1")
+		self.statusbar=self.wTree.get_widget("combobox1")
 		self.button=self.wTree.get_widget("button1")
 		self.message=self.wTree.get_widget("entry1")
 		self.chatwindow=self.wTree.get_widget("treeview2")
 		self.progress=self.wTree.get_widget("progressbar1")
 		self.desc=self.wTree.get_widget("entry2")
-
-		#tworzymy słownik par - "sygnał":funkcja
+		
+		#ustawienie statusow
+		self.statuslist=gtk.ListStore(str)
+		self.statusbar.set_model(self.statuslist)
+		self.statuslist.append(["Dostepny"])
+		self.statuslist.append(["Zaraz wracam"])
+		self.statuslist.append(["Wrócę później"])
+		self.statuslist.append(["Nie przeszkadzać"])
+		self.statuslist.append(["Chcę pogadać"])
+		self.statuslist.append(["Niewidoczny"])
+		
+		#wyswietlanie statusow
+		cell = gtk.CellRendererText()
+		self.statusbar.pack_start(cell)
+		self.statusbar.add_attribute(cell, 'text', 0)
+		
+		
+		#sygnaly
 		dic={
 		"send": self.send,
-		"chdesc": self.chdesc
+		"chdesc": self.chdesc,
+		"chstatus": self.chstatus
 		}
 		
 	#---------Skroty klawiszowe--------------------------------------
@@ -38,9 +57,8 @@ class okno:
 		self.message.connect("key_press_event", keys.message,self)
 		self.desc.connect("key_press_event", keys.status,self)
 	
-	#----------------------------------------------------------------
+	#---Stworzenie modelu dla wyswietlania rozmow----------------------
 	
-		
 		self.chat=gtk.ListStore(str)
 		self.chatwindow.set_model(self.chat)
 		renderer=gtk.CellRendererText()
@@ -59,7 +77,10 @@ class okno:
 	def chdesc(self,*widget):
 		desc=self.desc.get_text()
 		self.connection.set_desc(desc)
-
+	def chstatus(self,widget):
+		desc=self.desc.get_text()
+		index=self.statusbar.get_active()
+		self.connection.set_status(index,desc)
 
 				
 if __name__ == "__main__":
--- connection.py
+++ connection.py
@@ -2,20 +2,35 @@
 #!/usr/bin/env python
 import pygtk,gtk,xmpp,threading,time,xmpp,gobject,sys
 
+
+#----------klasa do zarzadzania polaczeniem-------------------------#
+
 class connection(threading.Thread):
 	def __init__(self,guiclass):
-		#self.gui=guiclass
+
+		#przypisanie paru zmiennych z glownej klasy
+
 		self.desc=guiclass.desc
 		self.chat=guiclass.chat
+		self.statusbar=guiclass.statusbar
+		self.progress=guiclass.progress
+		
+        #rozpoczecie watku polaczenia
+	
 		threading.Thread.__init__(self)
 		threading.Thread(target=self.connecting,args=()).start()
-		self.progress=guiclass.progress
-
+		
+#-------lączenie z serwerem-----------------------------------------#
+		
 	def connecting(self):
 		self.ifrun=True
+		#start watku z progressbarem
+		
 		threading.Thread(target=self.connectbar,args=()).start()
-
-		jid = 'pybberclient@gmail.com' # @gmail.com 
+		
+		#todo - dane do polaczenia pobrac z kreatora       !!!
+		
+		jid = 'pybberclient@gmail.com' 
 		pwd   = 'pybberjabber'
 		jid=xmpp.protocol.JID(jid)  
 		self.cl=xmpp.Client(jid.getDomain(),debug=[])
@@ -25,11 +40,21 @@ class connection(threading.Thread):
 		if self.cl.auth(jid.getNode(),pwd) == None: 
 			print "authentication failed"
 			sys.exit(0)
+			
+		#handler do odbierania wiadomosci
+		
 		self.cl.RegisterHandler('message',self.messageCB)
+		
+		#wylaczenie progressbara
 		self.ifrun=False
+		
+		#ustawienie poczatkowego statusu
 		self.cl.sendInitPresence()
 		self.cl.send(xmpp.dispatcher.Presence(priority=5, show=None,status="Pybber test"))
 		self.desc.set_text("Pybber test")
+		self.statusbar.set_active(0)
+		
+		#to rowniez do odbierania wiadomosci
 		self.GoOn(self.cl)
 		
 	def connectbar(self):
@@ -43,14 +68,38 @@ class connection(threading.Thread):
 		
 	def set_desc(self,desc):
 		self.cl.send(xmpp.dispatcher.Presence(priority=5, show=None,status=desc))
+	def set_status(self,index,desc):
+		if index==0:
+			self.cl.send(xmpp.dispatcher.Presence(show=None,status=desc))
+		if index==1:
+			self.cl.send(xmpp.dispatcher.Presence(show="away",status=desc))
+		if index==2:
+			self.cl.send(xmpp.dispatcher.Presence(show="xa",status=desc))
+		if index==3:
+			self.cl.send(xmpp.dispatcher.Presence(show="dnd",status=desc))
+		if index==4:
+			self.cl.send(xmpp.dispatcher.Presence(show="chat",status=desc))
+		if index==5:
+			self.cl.send(xmpp.dispatcher.Presence(show="unavailable",status=desc))
+		
+
+#------------Odbieranie wiadomosci:-------------------------------------
 
 	def messageCB(self,conn,mess):
 		text=mess.getBody()
 		user=mess.getFrom()
+		
+		#pobranie usera i tresci nadchodzacej rozmowy
+		
 		if text!=None:
 			user=user.getStripped()
+			
+			#wypisywanie tresci w oknie
+			
 			self.chat.append([user+": "+text])
-		
+
+
+	#funkcje sledzace wiadomosci:
 	def StepOn(self, conn):
 		try:
 			conn.Process(1)
