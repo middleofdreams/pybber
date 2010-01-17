8669efc2a9435aac5aa5e79a01aaead15fe23e04
Kuba <middleofdreams@gmail.com>
2010-01-17 22:04:04 +0100

    wycinanie wszystkich tagow

--- send.py
+++ send.py
@@ -9,21 +9,9 @@ def sendmsg(guiclass):
 	start_iter=buffer.get_start_iter()
 	end_iter=buffer.get_end_iter()
 	msg=buffer.get_text(start_iter, end_iter, include_hidden_chars=True)
-	#buffer.select_range(end_iter,start_iter)
 	buffer.set_text("")
-	#buffer.backspace(end_iter, False, True)
-	#buffer.backspace(start_iter, False, True)
-	start_iter=buffer.get_start_iter()
-	end_iter=buffer.get_end_iter()
-	buffer.delete(start_iter,end_iter)
-	#buffer.backspace(end_iter, False, True)
-	#buffer.backspace(start_iter, False, True)
-	buffer.place_cursor(buffer.get_iter_at_line_offset(0,0))
-	start, end = buffer.get_bounds()
-	buffer.select_range(start,start)
-	#buffer.backspace(buffer.get_end_iter(), True, True)
-	#buffer.delete_interactive(buffer.get_start_iter(),buffer.get_end_iter(),True)
-	
+	msg=msg.replace("<","&lt;")
+	msg=msg.replace("<","&gt;")
 
 	# msg=str("ME:"+msg)
 	if msg!="":
